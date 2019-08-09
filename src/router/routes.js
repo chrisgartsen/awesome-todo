0321e5b78bc55b108ea053ab3c218d4c6f8e039a
Chris Gartsen <chris@gartsen.nl>
2019-08-09 09:31:28 +0200

    Set initial pages and routing

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,10 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MyLayout.vue'),
+    component: () => import('layouts/layout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '/', component: () => import('pages/page-todo.vue') },
+      { path: '/settings', component: () => import('pages/page-settings.vue') }
     ]
   }
 ]
