9323d925963f80b317b779acdb081888cc943dc3
Chris Gartsen <chris@gartsen.nl>
2019-08-18 16:39:26 +0200

    Set login error dialog styling

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .auth-error {
+    color: red;
+  }
+</style>
--- src/utils/function-show-error.js
+++ src/utils/function-show-error.js
@@ -3,6 +3,10 @@ import { Dialog } from 'quasar'
 export function showErrorMessage(errorMessage) {
   Dialog.create({ 
     title: 'Error',
+    ok: {
+      color: 'red'
+    },  
+    class: 'auth-error',
     message: errorMessage
   })
 }
